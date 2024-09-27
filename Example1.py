30f621f13e2803f83d513adc35f42f30adf7a022
EricNafstad <eric.nafstad@student.nhh.no>
2024-09-27 09:41:17 +0200

    Example1 added

---
+++ Example1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 13:41:16 2024
+
+@author: ericnafstad
+"""
+
+"""
+Part 2, Lecture 1, Example 1
+
+Implement and test argmax() function that returns the location of a maximum.
+"""
+
+
+def argmax(values):
+    """
+    Return the location and value of the maximum contained in a given sequence.
+
+    Parameters
+    ----------
+    values : Sequence of numbers
+
+    Returns
+    -------
+    imax : int
+        Location of the maximum
+    vmax : int or float
+        Maximum value
+    """
+
+    # ADD YOUR IMPLEMENTATION HERE
+
+
+    N = len(values)
+    
+    imax = 0
+    vmax = values[0]
+    
+    for i in range(1, N):
+        v = values[i]
+        if v > vmax:
+            imax = i
+            vmax = v
+    
+    return imax, vmax
+
+
+def main():
+    # Create list of values to test argmax()
+    values = [2, 3, -1, 7, 4]
+    
+    # Use argmax() to locate the maximum
+    imax, vmax = argmax(values)
+    
+    # Print the results
+    print(f"The maximum value is {vmax} at index {imax}")
+
+if __name__ == '__main__':
+    main()
